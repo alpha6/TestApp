4a3af137bbef056196d51356952cd5fc49270a67
Denis Fedoseev <denis@fedoseev.work>
2023-08-08 10:42:32 +0300

    schema

---
+++ sql/00001_schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
+    `created` DATETIME NOT NULL,
+    `id` varchar(128) NOT NULL,
+    `int_id` char(16) NOT NULL,
+    `str` varchar(255) NOT NULL,
+    `status` BOOLEAN,
+    CONSTRAINT message_id_pk PRIMARY KEY(`id`)
+);
+CREATE INDEX message_created_idx ON message (created);
+CREATE INDEX message_int_id_idx ON message (int_id);
+
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE log (
+    `created` DATETIME NOT NULL,
+    `int_id` CHAR(16) NOT NULL,
+    `str` VARCHAR(255),
+    `address` VARCHAR(255)
+);
+CREATE INDEX log_address_idx ON `log` (`address`) USING HASH;
