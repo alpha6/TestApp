38cf89080e2d4600492f303fabce530170e1dd1c
Denis Fedoseev <denis@fedoseev.work>
2023-08-10 16:09:16 +0300

    move base to port 33060

--- start.sh
+++ start.sh
@@ -4,12 +4,13 @@ DB_HOST=127.0.0.1
 DB_NAME=test_base
 DB_USER=root
 DB_PASSWORD=Qwerty123~
+DB_PORT=33060
 
-docker run --rm -p 3306:3306 -e MYSQL_DATABASE=${DB_NAME} -e MYSQL_ROOT_PASSWORD=${DB_PASSWORD} -d mysql:8.1
+docker run --rm -p ${DB_PORT}:3306 -e MYSQL_DATABASE=${DB_NAME} -e MYSQL_ROOT_PASSWORD=${DB_PASSWORD} -d mysql:8.1
 
 echo 'Waiting for mysql ...'
 counter=1
-while ! mysql --protocol TCP --host=${DB_HOST} -u${DB_USER} -p${DB_PASSWORD} -e "show databases;" > /dev/null 2>&1; do
+while ! mysql --protocol TCP --host=${DB_HOST} --port=${DB_PORT} -u${DB_USER} -p${DB_PASSWORD} -e "show databases;" > /dev/null 2>&1; do
     sleep 1
     counter=`expr $counter + 1`
     if [ $counter -gt 60 ]; then
@@ -20,6 +21,6 @@ done
 
 echo 'Creating tables ...'
 
-mysql --host=${DB_HOST} -u${DB_USER} -p${DB_PASSWORD} ${DB_NAME} < ./sql/00001_schema.sql
+mysql --host=${DB_HOST} --port=${DB_PORT} -u${DB_USER} -p${DB_PASSWORD} ${DB_NAME} < ./sql/00001_schema.sql
 
-mysql --host=${DB_HOST} -u${DB_USER} -p${DB_PASSWORD} ${DB_NAME} -e 'show tables';
+echo 'Done'
